9e56fe5c6443afb0b67dc89554986f6cf90c1240
panyunyi <panyunyi@126.com>
2017-02-11 15:39:17 +0800

    农事记录增删查改

--- routes/datatable.js
+++ routes/datatable.js
@@ -2,6 +2,7 @@
 var router = require('express').Router();
 var AV = require('leanengine');
 var async=require('async');
+var moment=require('moment');
 
 router.get('/greenhouse', function(req, res, next) {
     let query = new AV.Query('greenhouse');
@@ -278,18 +279,37 @@ router.get('/farming', function(req, res, next) {
     function promise1(callback){
         let query = new AV.Query('farming');
         query.equalTo('isDel',false);
+        query.descending('startTime');
         query.include('greenhouse');
         query.include('crop');
+        query.include('user');
         query.find().then(function(results){
             async.map(results,function(result,callback1){
+                let startTime=new moment(result.get('startTime'));
+                let endTime=new moment(result.get('endTime'));
+                let totalseconds=endTime.diff(startTime)/1000;
+                let minutes=totalseconds/60;
+                let seconds=minutes%60;
+                let spendTime=minutes+"分"+seconds+"秒";
                 result.set('DT_RowId',result.id);
                 result.set('name',result.get('name')?result.get('name'):"");
-                result.set('status',result.get('status')?"已完成":"进行中");
+                result.set('startTime',startTime.format('YYYY-MM-DD HH:mm:ss'));
+                if(result.get('status')){
+                    result.set('spendTime',spendTime);
+                    result.set('endTime',endTime.format('YYYY-MM-DD HH:mm:ss'));
+                }else{
+                    result.set('spendTime',"");
+                    result.set('endTime',"");
+                }
+                result.set('info',result.get('info')?result.get('info'):"");
+                result.set('status',result.get('status'));
                 result.set('remark',result.get('remark')?result.get('remark'):"");
-                //result.set('greenhousename',result.get('greenhouse').get('name'));
-                //result.set('greenhouse',result.get('greenhouse').id);
-                //result.set('crop',result.get('crop').id);
-                //result.set('cropname',result.get('crop').get('name'));
+                result.set('greenhousename',result.get('greenhouse').get('name'));
+                result.set('greenhouse',result.get('greenhouse').id);
+                result.set('cropname',result.get('crop').get('name'));
+                result.set('crop',result.get('crop').id);
+                result.set('username',result.get('user').get('name'));
+                result.set('user',result.get('user').id);
                 callback1(null,result);
             },function(err,data){
                 resdata["data"]=data;
@@ -325,6 +345,34 @@ router.get('/farming', function(req, res, next) {
             });
         });
     }
+    function promise4(callback){
+        let query=new AV.Query('_User');
+        query.equalTo('isDel',false);
+        query.ascending('name');
+        query.find().then(function(results){
+            async.map(results,function(result,callback1){
+                result.set('label',result.get('name'));
+                result.set('value',result.id);
+                callback1(null,result);
+            },function(err,data){
+                callback(null,data);
+            });
+        });
+    }
+    function promise5(callback){
+        let query=new AV.Query('farmoption');
+        query.equalTo('isDel',false);
+        query.ascending('createdAt');
+        query.find().then(function(results){
+            async.map(results,function(result,callback1){
+                result.set('label',result.get('name'));
+                result.set('value',result.get('name'));
+                callback1(null,result);
+            },function(err,data){
+                callback(null,data);
+            });
+        });
+    }
     async.parallel([
         function (callback){
             promise1(callback);
@@ -334,8 +382,15 @@ router.get('/farming', function(req, res, next) {
         },
         function (callback){
             promise3(callback);
+        },
+        function (callback){
+            promise4(callback);
+        },
+        function (callback){
+            promise5(callback);
         }],function(err,results){
-            resdata["options"]=Object.assign({"greenhouse":results[1]},{"crop":results[2]});
+            resdata["options"]=Object.assign({"greenhouse":results[1]},{"crop":results[2]},
+            {"user":results[3]},{"farm":results[4]});
             res.jsonp(resdata);
     });
 });
@@ -343,23 +398,73 @@ router.get('/farming', function(req, res, next) {
 var Farming = AV.Object.extend('farming');
 router.post('/farming/add',function(req,res){
     var arr=req.body;
-    var crop=AV.Object.createWithoutData('farming', id);
-    crop.set('name',arr['data[0][name]']);
-    crop.set('info',arr['data[0][info]']);
-    crop.set('nutrition',arr['data[0][nutrition]']);
-    crop.set('cooking',arr['data[0][cooking]']);
+    var farming=new Farming();
+    farming.set('name',arr['data[0][farm]']);
+    farming.set('info',arr['data[0][info]']);
+    farming.set('startTime',new Date(arr['data[0][startTime]']));
+    farming.set('endTime',new Date(arr['data[0][endTime]']));
     var greenhouse=AV.Object.createWithoutData('greenhouse', arr['data[0][greenhouse]']);
-    crop.set('greenhouse',greenhouse);
-    crop.set('isDel',false);
-    crop.save().then(function(pro){
+    var crop=AV.Object.createWithoutData('crop', arr['data[0][crop]']);
+    var user=AV.Object.createWithoutData('_User', arr['data[0][user]']);
+    farming.set('greenhouse',greenhouse);
+    farming.set('crop',crop);
+    farming.set('user',user);
+    farming.set('status',Boolean(arr['data[0][status]']*1));
+    farming.set('remark',arr['data[0][remark]']);
+    farming.set('isDel',false);
+    farming.save().then(function(fa){
         var data=[];
-        pro.set('DT_RowId',pro.id);
-        greenhouse.fetch().then(function(){
-            pro.set('greenhouse',greenhouse.id);
-            pro.set('greenhousename',greenhouse.get('name'));
-            data.push(pro);
-            res.jsonp({"data":data});
+        let startTime=new moment(fa.get('startTime'));
+        let endTime=new moment(fa.get('endTime'));
+        let totalseconds=endTime.diff(startTime)/1000;
+        let minutes=totalseconds/60;
+        let seconds=minutes%60;
+        let spendTime=minutes+"分"+seconds+"秒";
+        fa.set('DT_RowId',fa.id);
+        fa.set('remark',fa.get('remark')?fa.get('remark'):"");
+        fa.set('startTime',startTime.format('YYYY-MM-DD HH:mm:ss'));
+        if(fa.get('status')){
+            fa.set('endTime',endTime.format('YYYY-MM-DD HH:mm:ss'));
+            fa.set('spendTime',spendTime);
+        }else{
+            fa.set('endTime',"");
+            fa.set('spendTime',"");
+        }
+        function promise1(callback){
+            greenhouse.fetch().then(function(){
+                fa.set('greenhouse',greenhouse.id);
+                fa.set('greenhousename',greenhouse.get('name'));
+                callback(null,greenhouse.id);
+            });
+        }
+        function promise2(callback){
+            crop.fetch().then(function(){
+                fa.set('crop',crop.id);
+                fa.set('cropname',crop.get('name'));
+                callback(null,crop.id);
+            });
+        }
+        function promise3(callback){
+            user.fetch().then(function(){
+                fa.set('user',user.id);
+                fa.set('username',user.get('name'));
+                callback(null,user.id);
+            });
+        }
+        async.parallel([
+            function (callback){
+                promise1(callback);
+            },
+            function (callback){
+                promise2(callback);
+            },
+            function (callback){
+                promise3(callback);
+            }],function(err,results){
+                data.push(fa);
+                res.jsonp({"data":data});
         });
+
     },function(error){
         console.log(error);
     });
@@ -368,23 +473,73 @@ router.post('/farming/add',function(req,res){
 router.put('/farming/edit/:id',function(req,res){
     var arr=req.body;
     var id=req.params.id;
-    var crop=new Crop();
-    crop.set('name',arr['data['+id+'][name]']);
-    crop.set('info',arr['data['+id+'][info]']);
-    crop.set('nutrition',arr['data['+id+'][nutrition]']);
-    crop.set('cooking',arr['data['+id+'][cooking]']);
-    var farming=AV.Object.createWithoutData('farming', arr['data['+id+'][farming]']);
-    crop.set('farming',farming);
-    crop.set('isDel',false);
-    crop.save().then(function(pro){
+    var farming=AV.Object.createWithoutData('farming', id);
+    farming.set('name',arr['data['+id+'][farm]']);
+    farming.set('info',arr['data['+id+'][info]']);
+    farming.set('startTime',new Date(arr['data['+id+'][startTime]']));
+    farming.set('endTime',new Date(arr['data['+id+'][endTime]']));
+    var greenhouse=AV.Object.createWithoutData('greenhouse', arr['data['+id+'][greenhouse]']);
+    var crop=AV.Object.createWithoutData('crop', arr['data['+id+'][crop]']);
+    var user=AV.Object.createWithoutData('_User', arr['data['+id+'][user]']);
+    farming.set('greenhouse',greenhouse);
+    farming.set('crop',crop);
+    farming.set('user',user);
+    farming.set('status',Boolean(arr['data['+id+'][status]']*1));
+    farming.set('remark',arr['data['+id+'][remark]']);
+    farming.set('isDel',false);
+    farming.save().then(function(fa){
         var data=[];
-        pro.set('DT_RowId',pro.id);
-        greenhouse.fetch().then(function(){
-            pro.set('greenhouse',greenhouse.id);
-            pro.set('greenhousename',greenhouse.get('name'));
-            data.push(pro);
-            res.jsonp({"data":data});
+        let startTime=new moment(fa.get('startTime'));
+        let endTime=new moment(fa.get('endTime'));
+        let totalseconds=endTime.diff(startTime)/1000;
+        let minutes=totalseconds/60;
+        let seconds=minutes%60;
+        let spendTime=minutes+"分"+seconds+"秒";
+        fa.set('DT_RowId',fa.id);
+        fa.set('remark',fa.get('remark')?fa.get('remark'):"");
+        fa.set('startTime',startTime.format('YYYY-MM-DD HH:mm:ss'));
+        if(fa.get('status')){
+            fa.set('endTime',endTime.format('YYYY-MM-DD HH:mm:ss'));
+            fa.set('spendTime',spendTime);
+        }else{
+            fa.set('endTime',"");
+            fa.set('spendTime',"");
+        }
+        function promise1(callback){
+            greenhouse.fetch().then(function(){
+                fa.set('greenhouse',greenhouse.id);
+                fa.set('greenhousename',greenhouse.get('name'));
+                callback(null,greenhouse.id);
+            });
+        }
+        function promise2(callback){
+            crop.fetch().then(function(){
+                fa.set('crop',crop.id);
+                fa.set('cropname',crop.get('name'));
+                callback(null,crop.id);
+            });
+        }
+        function promise3(callback){
+            user.fetch().then(function(){
+                fa.set('user',user.id);
+                fa.set('username',user.get('name'));
+                callback(null,user.id);
+            });
+        }
+        async.parallel([
+            function (callback){
+                promise1(callback);
+            },
+            function (callback){
+                promise2(callback);
+            },
+            function (callback){
+                promise3(callback);
+            }],function(err,results){
+                data.push(fa);
+                res.jsonp({"data":data});
         });
+
     },function(error){
         console.log(error);
     });
@@ -392,9 +547,9 @@ router.put('/farming/edit/:id',function(req,res){
 
 router.delete('/farming/remove/:id',function(req,res){
     var id=req.params.id;
-    var crop = AV.Object.createWithoutData('farming', id);
-    crop.set('isDel',true);
-    crop .save().then(function(){
+    var farming = AV.Object.createWithoutData('farming', id);
+    farming.set('isDel',true);
+    farming .save().then(function(){
         res.jsonp({"data":[]});
     });
 });
